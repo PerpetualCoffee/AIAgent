8d2b0962c38ecbea62d61ac361da7e8915126af7
PerpetualCoffee <darth.cithine@gmail.com>
2026-05-02 22:15:10 +1000

    completed module 1. LLMs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     load_dotenv()
@@ -9,8 +12,25 @@ def main():
         raise RuntimeError("API_KEY not found!")
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="Please enter your single-line prompt.")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access 'args.user_prompt'
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
+    if response.usage_metadata is None:
+        raise RuntimeError("the API request has failed!")
+    else:
+        if args.verbose:
+            print(F"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
